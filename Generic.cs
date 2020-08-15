fbda542cc3af132083f14cdbdabb46ed8c1277b3
vince-huynh <vince.bhuynh@gmail.com>
2020-08-15 10:19:23 -0400

    question 21

---
+++ Generic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// question 21
+// our valuetype will have Value
+namespace Final022
+{
+    class Generic<T> where T: struct
+    {
+        private ICollection<T> items;
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public T getSingle(params Value)
+        {
+            return items.FirstOrDefault(search);
+        }
+
+        public ICollection<T> sortDescending()
+        {
+            return items.OrderByDescending(c => c.Value);
+        }
+    }
+
+ 
+}
